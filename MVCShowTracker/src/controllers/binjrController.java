1fb2e81501a54ba28a4246befe66b8c66d8ea78f
Robert Helmick <roberthelmick@Roberts-MacBook-Pro.local>
2017-02-12 12:23:08 -0700

    addTVShow, addSeason and addEpisode methods added to controller

--- MVCShowTracker/src/controllers/binjrController.java
+++ MVCShowTracker/src/controllers/binjrController.java
@@ -8,17 +8,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import data.AdminDAO;
 import data.ClientDAO;
+import entities.Episode;
+import entities.Season;
+import entities.TVShow;
 import entities.User;
 
 @Controller
 public class binjrController {
 	private ClientDAO cDao = null;
 	private AdminDAO aDao = null;
-	
-	
-	@RequestMapping(path="login.do")
-	public String userLogin(@RequestParam(name="username") String username,
-			@RequestParam(name="password") String password, HttpSession session) {
+
+	@RequestMapping(path = "login.do")
+	public String userLogin(@RequestParam(name = "username") String username,
+			@RequestParam(name = "password") String password, HttpSession session) {
 		User user = null;
 		try {
 			user = cDao.getUser(username, password);
@@ -30,9 +32,9 @@ public class binjrController {
 		return "profileSplash.jsp";
 	}
 
-	@RequestMapping(path="addUser.do")
-	public String addUser(@RequestParam(name="passwordConfirm") String passwordConfirm,
-			User user, HttpSession session) {
+	@RequestMapping(path = "addUser.do")
+	public String addUser(@RequestParam(name = "passwordConfirm") String passwordConfirm, User user,
+			HttpSession session) {
 		if (user.getPassword() == passwordConfirm) {
 			try {
 				cDao.createUser(user);
@@ -45,9 +47,68 @@ public class binjrController {
 		return "index.jsp";
 	}
 
-	@RequestMapping(path="goHome.do")
+	@RequestMapping(path = "goHome.do")
 	public String goHome(HttpSession session) {
 		return "profileSplash.jsp";
 	}
 
+	@RequestMapping(path = "addShow.do")
+	public String addTVShow(TVShow tvshow, HttpSession session) {
+		try {
+			aDao.addTVShow(tvshow);
+		} catch (Exception e) {
+			return "error.jsp";
+		}
+		return "addSeason.jsp";
+	}
+
+	@RequestMapping(path = "deleteShow.do")
+	public String deleteTVShow(TVShow tvshow, HttpSession session) {
+		try {
+			aDao.deleteTVShow(tvshow);
+		} catch (Exception e) {
+			return "error.jsp";
+		}
+		return "addSeason.jsp";
+	}
+
+	@RequestMapping(path = "addSeason.do")
+	public String addSeason(Season season, HttpSession session) {
+		try {
+			aDao.addSeason(season);
+		} catch (Exception e) {
+			return "error.jsp";
+		}
+		return "addEpisode.jsp";
+	}
+	
+	@RequestMapping(path = "deleteSeason.do")
+	public String deleteSeason(Season season, HttpSession session) {
+		try {
+			aDao.deleteSeason(season);
+		} catch (Exception e) {
+			return "error.jsp";
+		}
+		return "addEpisode.jsp";
+	}
+
+	@RequestMapping(path = "addEpisode.do")
+	public String addEpisode(Episode episode, HttpSession session) {
+		try {
+			aDao.addEpisode(episode);
+		} catch (Exception e) {
+			return "error.jsp";
+		}
+		return "profileSplash.jsp";
+	}
+
+	@RequestMapping(path = "deleteEpisode.do")
+	public String deleteEpisode(Episode episode, HttpSession session) {
+		try {
+			aDao.deleteEpisode(episode);
+		} catch (Exception e) {
+			return "error.jsp";
+		}
+		return "profileSplash.jsp";
+	}
 }
