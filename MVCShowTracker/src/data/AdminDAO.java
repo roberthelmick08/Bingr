a3971c2fad8b2dd4875cdc7c2fc0723f18ba9fdb
Nicholas Marchese <NikM@NikMs-MacBook-Pro.local>
2017-02-13 10:59:48 -0700

    continueing to add functionality to admin jsp files

--- MVCShowTracker/src/data/AdminDAO.java
+++ MVCShowTracker/src/data/AdminDAO.java
@@ -7,9 +7,15 @@ import entities.TVShow;
 public interface AdminDAO {
 	TVShow addTVShow(TVShow tvShow);
 	
+	TVShow getTVShowById(int id);
+	
 	Season addSeason(Integer tvShowId, Season season);
 	
+	Season getSeasonById(int id);
+	
 	Episode addEpisode(Integer seasonId, Episode episode);
+	
+	Episode getEpisodeById(int id);
 	
 	boolean removeTVShow(int id);
 	
